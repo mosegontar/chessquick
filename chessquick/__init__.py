c71fd8e0c399b3343b827146aa2d1c282e21ad7a
Alex Gontar <mosegontar@gmail.com>
2016-10-02 10:41:18 -0400

    Add support for migrations using Flask-Migrate

--- chessquick/__init__.py
+++ chessquick/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
 app.static_folder = 'static'
 app.config.from_object('config')
 
 db = SQLAlchemy(app)
+
+
 from chessquick import views, models
--- create_db.py
+++
@@ -1,3 +0,0 @@
-from chessquick import db
-
-db.create_all()
