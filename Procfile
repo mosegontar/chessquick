e20cc4c7e3bbb30426876222569ad79708cf92bd
Alex Gontar <mosegontar@gmail.com>
2016-10-23 15:33:50 -0400

    Add procfile and update config

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn chessquick:app --log-file=-
--- config.py
+++ config.py
@@ -5,26 +5,31 @@ import authomatic
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 STARTING_FEN_STRING = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
-DEBUG = True
-SECRET_KEY = 'quiet'
+DEBUG = False
+SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = 'postgresql://user:pass@localhost/app'
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', default="sqlite:///")
 
 # http://exploreflask.com/en/latest/users.html?highlight=utils
 BCRYPT_LOG_ROUNDS = 12
 
 AUTHOMATIC_SECRET = 'some secret'
 AUTHOMATIC_CONFIG = {
-        'twitter': {
-
-            'class_': oauth1.Twitter,
-            'consumer_key': 'coupons',
-            'consumer_secret': 'money helps}'
-        },
         'google': {
             'class_': oauth2.Google,
-            'consumer_key': 'https://en.wikipedia.org/wiki/Consumerism',
-            'consumer_secret': 'the secret is you',
-            'scope': 'it out' # https://developers.google.com/identity/protocols/googlescopes
+            'consumer_key': os.environ.get('GOOGLE_CONSUMER_KEY'),
+            'consumer_secret': os.environ.get('GOOGLE_CONSUMER_SECRET'),
+            'scope': ['profile', 'email']
         },
 }
+
+# mail server settings
+MAIL_SERVER = os.environ.get('MAIL_SERVER')
+MAIL_PORT = os.environ.get('MAIL_PORT')
+MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
+MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL')
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
+# Admins
+ADMINS = [os.environ.get('ADMIN_EMAIL')]
