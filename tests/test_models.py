30b7fba75395eb848f95ef97f71d84860e50258d
Alex Gontar <mosegontar@gmail.com>
2017-01-22 14:34:20 -0500

    Break up test_models into classes for each model

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,8 +1,7 @@
 from .test_base import BaseTestCase, FIRST_MOVE_FEN, SECOND_MOVE_FEN
 from chessquick.models import Matches, Rounds
 
-
-class TestModels(BaseTestCase):
+class TestRoundsModel(BaseTestCase):
 
     def test_new_round_added_to_database_successfully(self):
         self.add_new_round(FIRST_MOVE_FEN)
@@ -10,6 +9,19 @@ class TestModels(BaseTestCase):
         self.assertIn(1, round_nums)
         self.assertIn(0, round_nums)
         self.assertEqual(Rounds.query.all()[0].date_of_turn, Rounds.query.all()[1].date_of_turn)
+
+    def test_Rounds_add_turn_to_game_creates_new_match_if_no_URL_passed(self):
+        self.assertEqual(len(Matches.query.all()), 0)
+        self.add_new_round(FIRST_MOVE_FEN)
+        self.assertEqual(len(Matches.query.all()), 1)
+
+    def test_newly_added_rounds_associated_with_matches_object(self):
+        self.assertEqual(len(Matches.query.all()), 0)
+        self.add_new_round(FIRST_MOVE_FEN)
+        match = Matches.query.first()
+        self.assertEqual(len(match.rounds.all()), 2)        
+
+class TestMatchesModel(BaseTestCase):
 
     def test_get_match_by_url_method_returns_None_if_passed_empty_string(self):
         match = Matches.get_match_by_url('')
@@ -24,19 +36,7 @@ class TestModels(BaseTestCase):
         self.assertIn(searched_match, Matches.query.all())
         self.assertEqual(searched_match.match_url, match2_url)
 
-    def test_Rounds_add_turn_to_game_creates_new_match_if_no_URL_passed(self):
-        self.assertEqual(len(Matches.query.all()), 0)
-        self.add_new_round(FIRST_MOVE_FEN)
-        self.assertEqual(len(Matches.query.all()), 1)
-
     def test_Matches_start_new_match_creates_correct_length_random_match_url(self):
         self.add_new_round(FIRST_MOVE_FEN)
         self.assertEqual(len(Matches.query.first().match_url), 8)
 
-    def test_newly_added_rounds_associated_with_matches_object(self):
-        self.assertEqual(len(Matches.query.all()), 0)
-        self.add_new_round(FIRST_MOVE_FEN)
-        self.assertEqual(len(Matches.query.all()), 1)
-        
-        match = Matches.query.first()
-        self.assertEqual(len(match.rounds.all()), 2)
