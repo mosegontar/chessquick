1f5c674aebb729a2621738ca7c2e80545c1f9495
Alex Gontar <mosegontar@gmail.com>
2017-01-22 20:39:32 -0500

    Test user can unsave game via  OptionToggler.unsave_game

--- tests/test_viewmodel.py
+++ tests/test_viewmodel.py
@@ -5,7 +5,7 @@ from chessquick.viewmodel import OptionToggler
 
 class TestOptionToggler(BaseTestCase):
 
-    def test_can_save_game_with_OptionToggler(self):
+    def test_can_save_open_game(self):
 
         user = self.add_fake_users(1)[0]
         _match_url = self.add_new_round(FIRST_MOVE_FEN)
@@ -15,3 +15,43 @@ class TestOptionToggler(BaseTestCase):
         ot.save_game()
 
         self.assertEqual(user.matches.first(), match)
+        self.assertEqual(match.white_player, user)
+
+    def test_cannot_save_closed_game(self):
+
+        user1, user2, user3 = self.add_fake_users(3)
+        _match_url = self.add_new_round(FIRST_MOVE_FEN)
+        match = Matches.get_match_by_url(_match_url)
+
+        user1.save_match('w', match)      
+        user3.save_match('b', match)
+
+        ot = OptionToggler(user2, 'w', match)
+        ot.save_game()
+
+        self.assertIsNone(user2.matches.first())
+
+    def test_can_unsave_game(self):
+
+        user1, user2 = self.add_fake_users(2)
+        _match_url = self.add_new_round(FIRST_MOVE_FEN)
+        match = Matches.get_match_by_url(_match_url)
+
+        user1.save_match('w', match)      
+        user2.save_match('b', match)
+
+        self.assertEqual(user1.matches.first(), match)
+        self.assertEqual(user2.matches.first(), match)        
+        self.assertEqual(match.white_player, user1)        
+        self.assertEqual(match.black_player, user2)
+
+        ot = OptionToggler(user2, 'w', match)
+        ot.unsave_game()
+
+        self.assertEqual(user1.matches.first(), match)
+        self.assertNotEqual(user2.matches.first(), match)        
+        self.assertEqual(match.white_player, user1)        
+        self.assertNotEqual(match.black_player, user2)        
+
+
+
